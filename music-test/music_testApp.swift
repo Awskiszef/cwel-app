05dacc939b9c7793d635a9bf29cadb46232ccde4
Antoni Dolatowski <dolatowskiantek2137@gmail.com>
2026-01-29 22:25:36 +0100

    Initial Commit

---
+++ music-test/music_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  music_testApp.swift
+//  music-test
+//
+//  Created by Antoni Dolatowski on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct music_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
